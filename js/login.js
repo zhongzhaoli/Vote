5dc7b42fbbd73050263ad766e5f40dd1dedfc05d
Custer <525966315@qq.com>
2018-12-12 20:34:55 +0800

    统计图和index

--- js/login.js
+++ js/login.js
@@ -1,8 +1,11 @@
 $(function(){
+    //区分线上线上环境
     (typeof baseUrl !== 'undefined') ? api.url = baseUrl : api.url = "http://10.1.53.123:9999";
     $("[data-login]").on("click", function(){
         api.login_api($("[name='id']").val()).then((mes) => {
             console.log(mes);
+        }).catch((err) => {
+            $.growl.error({title: "错误标题", message: "错误消息内容!"});
         });
     });
 })
